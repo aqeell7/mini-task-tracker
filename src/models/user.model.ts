1b761e6f04384995792e9d7faf094facf287b12d
ditto-chaar <md.aqeelahmed007@gmail.com>
2026-02-15 16:57:02 +0530

    feat: add secure password hashing to user model
    - Implement bcryptjs for password encryption- Add pre-save hook to hash passwords automatically- Add comparePassword method for login verification

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,10 +1,12 @@
 import mongoose,{Schema, Document} from "mongoose";
+import bcrypt from "bcryptjs";
 
 export interface IUser extends Document{
   name: string,
   email: string,
   password?: string,
   createdAt: Date
+  comparePassword(candidatePassword: string): Promise<boolean>
 }
 
 const UserSchema: Schema = new Schema(
@@ -15,5 +17,30 @@ const UserSchema: Schema = new Schema(
   },
   {timestamps: true}
 )
+
+UserSchema.pre<IUser>("save", async function (){
+  if(!this.isModified("password")){
+    return;
+  }
+
+  try{
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password!, salt); 
+  }catch(err){
+    throw new Error("Failed to hash user password")
+  } 
+});
+
+UserSchema.methods.comparePassword = async function (
+  candidatePassword: string
+  ): Promise<boolean> {
+    if (!this.password) {
+      throw new Error("Password not loaded on user document");
+    }
+
+    const isMatch = await bcrypt.compare(candidatePassword, this.password);
+    return isMatch;
+};
+
 
 export default mongoose.model<IUser>("User",UserSchema)
